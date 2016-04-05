f74126934c5200f08b76072fd0400f0baf9bf790
Erik Lindqvist <eriksmejl@gmail.com>
2016-04-05 20:22:34 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -14,8 +14,25 @@ set -e
 
 DEPLOY_DIR=$1
 
+EXCLUDE_FILES="deploy.sh \
+              ISSUE_TEMPLATE.md \
+              CONTRIBUTING.md \
+              tests/.gitignore \
+              tests/testFM.py \
+              tests/testHelper.py \
+              tests/testSvt.py \
+              tests/lib/CommonFunctions.py \
+              tests/lib/xbmc.py \
+              tests/lib/xbmcaddon.py \
+              tests/lib/xbmcgui.py"
+
 FILES=`git ls-files`
 
 for FILE in $FILES; do
+  if [[ $EXCLUDE_FILES =~ $FILE ]]; then
+    echo "Skipping excluded file $FILE"
+    continue
+  fi
+  echo "Copying $FILE to $DEPLOY_DIR$FILE"
   cp --parent $FILE $DEPLOY_DIR
 done
