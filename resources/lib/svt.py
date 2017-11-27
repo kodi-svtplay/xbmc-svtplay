116bcce9e8d388b2edee0f0d5a28058bc975a07b
Erik Lindqvist <eriksmejl@gmail.com>
2017-11-27 22:18:08 +0100

    Trim request URL

--- resources/lib/svt.py
+++ resources/lib/svt.py
@@ -323,7 +323,7 @@ def getVideoJSON(video_url):
 def getItems(section_name, page):
   if not page:
     page = 1
-  json_data = __get_json(section_name+"?page="+str(page)+"&excludedTagsString=lokalt&accessServiceFilters=")
+  json_data = __get_json(section_name+"?page="+str(page)+"&excludedTagsString=lokalt")
   if json_data is None:
     return None
 
