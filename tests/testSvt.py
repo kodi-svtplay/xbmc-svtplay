49c1d33382b2e060038eb5432799adf24b5ce7e0
Erik Lindqvist <eriksmejl@gmail.com>
2017-12-31 17:28:32 +0100

    The great rewrite
    The reasons behind this rewrite is that much of the code is
    from when the plugin used to scrape the website.
    Since the transition to using APIs the most of the changes has
    been more or less hacks.
    
    This mononlith commit contains new code, clean up and removal of
    dead code.

--- tests/testSvt.py
+++ tests/testSvt.py
@@ -17,6 +17,8 @@ class TestSvtModule(unittest.TestCase):
   def assertHasContent(self, list):
     if list == None:
       self.fail("List is None")
+      return False
+    return True
 
   def assertHasContentStrict(self, list):
     if list == None:
@@ -26,96 +28,77 @@ class TestSvtModule(unittest.TestCase):
 
   def test_alphabetic(self):
     programs = svt.getAtoO()
-
     self.assertHasContent(programs)
-
     for program in programs:
       for key in program.keys():
         self.assertIsNotNone(program[key])
 
   def test_get_categories(self):
-
     categories = svt.getCategories()
-
     self.assertHasContentStrict(categories)
-
     for category in categories:
       for key in category.keys():
         self.assertIsNotNone(category[key])
 
   def test_get_programs_for_category(self):
-
     categories = svt.getCategories()
-    is_failing = False
+    has_failed = False
     for category in categories:
       programs = svt.getProgramsForGenre(category["genre"])
-
       if programs is None:
-        is_failing = True
+        has_failed = True
         continue
-
       for program in programs:
         for key in program.keys():
           self.assertIsNotNone(program[key])
-
-    if is_failing:
+    if has_failed:
       self.fail("Test failed due to fetch issues. See log above.")
 
   def test_get_alphas(self):
-
     alphas = svt.getAlphas()
-
     self.assertHasContentStrict(alphas)
 
   def test_get_programs_by_letter(self):
-
     letter = u'A' # "A" should always have programs...
-
     programs = svt.getProgramsByLetter(letter)
-
     self.assertHasContentStrict(programs)
-
     for program in programs:
       for key in program.keys():
         self.assertIsNotNone(program[key])
 
   def test_search_results(self):
-
-    search_term = "xgames"
+    search_term = "agenda"
     items = svt.getSearchResults(search_term)
-
     self.assertHasContent(items)
-
+    if len(items) < 1:
+        # the hard coded search term needs
+        # to be changed if it doesn't yield
+        # any results => raise error to alert
+        self.fail("search returned no results")
     for item in items:
       for key in item.keys():
         self.assertIsNotNone(item[key])
 
   def test_get_channels(self):
     items = svt.getChannels()
-
     self.assertHasContentStrict(items)
 
   def test_get_latest_news(self):
     items = svt.getLatestNews()
-
     self.assertHasContent(items)
 
   def test_get_episodes(self):
-    url = "/agenda"
-    articles = svt.getEpisodes(url)
-
+    slug = "agenda"
+    articles = svt.getEpisodes(slug)
     self.assertHasContentStrict(articles)
-
     for article in articles:
       for key in article.keys():
         self.assertHasContent(article[key])
 
   def test_get_clips(self):
-    url = "/sportnytt"
-    articles = svt.getClips(url)
-
+    slug = "sportnytt"
+    articles = svt.getClips(slug)
     self.assertHasContentStrict(articles)
-
     for article in articles:
       for key in article.keys():
         self.assertHasContent(article[key])
@@ -144,14 +127,20 @@ class TestSvtModule(unittest.TestCase):
   def test_get_video_json(self):
     items = svt.getAtoO()
     self.assertHasContent(items)
+    video_ok = False
     for item in items:
-      videos = svt.getEpisodes(item["url"])
-      self.assertHasContent(videos)
-      for video in videos:
-        json_obj = svt.getVideoJSON(video["url"])
-        self.assertHasContent(json_obj)
+      if video_ok:
         break
-      break
-    
+      if item["type"] != "program":
+          continue
+      slug = item["url"].split("/")[-1]
+      episodes = svt.getEpisodes(slug)
+      self.assertHasContent(episodes)
+      for episode in episodes:
+        json_obj = svt.getVideoJSON(episode["url"])
+        if self.assertHasContent(json_obj):
+          video_ok = True
+          break
+
 if __name__ == "__main__":
   unittest.main()
