1e29f415e4a6fcb095175d0d133f3983dad323c4
Erik Lindqvist <eriksmejl@gmail.com>
2019-09-19 13:47:19 +0200

    Add py2/py3 compability import

--- resources/lib/mode/common.py
+++ resources/lib/mode/common.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import,unicode_literals
 import xbmcgui # pylint: disable=import-error
 import xbmcplugin # pylint: disable=import-error
 
--- resources/lib/playback.py
+++ resources/lib/playback.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import,unicode_literals
 import time
 import xbmc # pylint: disable=import-error
 import xbmcgui # pylint: disable=import-error
