4919e833c4ed08e49ecde16b035c132bf3e04350
Erik Lindqvist <eriksmejl@gmail.com>
2019-12-05 14:22:23 +0100

    Fanart is now a an optinal property for all list items

--- resources/lib/listing/listitem.py
+++ resources/lib/listing/listitem.py
@@ -9,7 +9,7 @@ class PlayItem(object):
     VIDEO_ITEM = "video"
     SHOW_ITEM = "show"
 
-    def __init__(self, title, id, item_type, thumbnail="", geo_restricted=False, info={}):
+    def __init__(self, title, id, item_type, thumbnail="", geo_restricted=False, info={}, fanart=""):
         if not title:
             raise ValueError("Title is missing!")
         if not id:
@@ -22,18 +22,18 @@ class PlayItem(object):
         self.geo_restricted = geo_restricted
         self.item_type = item_type
         self.info = info
+        self.fanart = fanart
 
 class VideoItem(PlayItem):
     """
     A video list item.
     """
     def __init__(self, title, video_id, thumbnail, geo_restricted, info={}, fanart=""):
-        super(VideoItem, self).__init__(title, video_id, PlayItem.VIDEO_ITEM, thumbnail, geo_restricted, info)
-        self.fanart = fanart
+        super(VideoItem, self).__init__(title, video_id, PlayItem.VIDEO_ITEM, thumbnail, geo_restricted, info, fanart)
 
 class ShowItem(PlayItem):
     """
     A show list item, which means a folder containing VideoItems
     """
-    def __init__(self, title, show_id, thumbnail, geo_restricted):
-        super(ShowItem, self).__init__(title, show_id, PlayItem.SHOW_ITEM, thumbnail, geo_restricted, info={})
+    def __init__(self, title, show_id, thumbnail, geo_restricted, info={}, fanart=""):
+        super(ShowItem, self).__init__(title, show_id, PlayItem.SHOW_ITEM, thumbnail, geo_restricted, info, fanart)
