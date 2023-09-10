1057a7e717909fa330d38aeac2c1f79ab5f836db
linqcan <eriksmejl@gmail.com>
2023-09-10 22:46:12 +0200

    Fix genres

--- tests/testGraphQL.py
+++ tests/testGraphQL.py
@@ -51,7 +51,7 @@ class TestGraphQLModule(unittest.TestCase):
     
     def test_getProgramsForGenre(self):
         sut = graphql.GraphQL()
-        items = sut.getProgramsForGenre("drama")
+        items = sut.getProgramsForGenre("nyheter")
         self.assertItems(items)
     
     def test_get_video_content(self):
